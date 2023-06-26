4919e55bbcdd68864625292d86571d41f80b6340
delamoar <you@example.com>
2023-06-26 16:43:26 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,44 +1,41 @@
-let viki = "https://api.npoint.io/9cd2a33b98f11a0d0794/characters/0"
-
-
-
-// Your code here
-// Retreve data on the characters from the server
-fetch('http://localhost:3000/characters')
-  .then(response => response.json())
-  .then(characters => {
+window.onload = function() {
     const characterBar = document.getElementById('character-bar');
-  // Add character names to character bar
-  characters.forEach(character => {
-    const characterName = document.createElement('span');
-    characterName.textContent = character.name;
-    characterName.addEventListener('click', () => {
-      displayCharacterDetails(character);
-    });
-    characterBar.appendChild(characterName);
-  });
-})
-// To display character details in the detailed info section
-function displayCharacterDetails(character) {
-    const detailedInfo = document.getElementById('detailed-info');
-    const name = detailedInfo.querySelector('#name');
-    const image = detailedInfo.querySelector('#image');
-    const voteCount = detailedInfo.querySelector('#vote-count');
-    const votesForm = detailedInfo.querySelector('#votes-form'); 
-// put character details
-    name.textContent = character.name;
-    image.src = character.image;
-    image.alt = character.name;
-    voteCount.textContent = character.votes;
-//Votes submit event listener 
-  votesForm.addEventListener('submit', event => { 
-    event.preventDefault();
-    const votesInput = votesForm.querySelector('#votes');
-    const votes = parseInt(votesInput.value);
-    if (!isNaN(votes)) {
-      character.votes += votes;
-      voteCount.textContent = character.votes;
+    const characterImage = document.getElementById('image');
+    const characterName = document.getElementById('name');
+    const votesForm = document.getElementById('votes-form');
+    const votesInput = document.getElementById('votes');
+    const flataApi = "https://api.npoint.io/9cd2a33b98f11a0d0794/characters"
+    
+    fetch(`${flataApi}`)
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(character => {
+          const characterNameElement = document.createElement('p');
+          characterNameElement.textContent = character.name;
+          characterNameElement.addEventListener('click', () => {
+            characterImage.src = character.image;
+            characterImage.alt = character.name;
+            characterName.textContent = character.name;
+          });
+          characterBar.appendChild(characterNameElement);
+        });
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  
+    votesForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+  
+      const characterVotes = document.createElement('h1');
+      const characterVotesValue = parseInt(votesInput.value);
+      const characterVoteCount = characterVotesValue || 0;
+  
+      if (characterVotesValue) {
+        characterVotes.textContent = `Total Votes: ${characterVotesValue}`;
+        characterBar.appendChild(characterVotes);
+      }
+  
       votesInput.value = '';
-    }
-  });
-}
+    });
+  };
